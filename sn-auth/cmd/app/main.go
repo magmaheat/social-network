b5c5e088dff5f76b00cf0b754c1f62deeb21889c
Quinton Stephen <quintons@ge-e5.kzn.21-school.ru>
2024-09-14 15:22:52 +0300

    restart project

--- sn-auth/cmd/app/main.go
+++
@@ -1,7 +0,0 @@
-package main
-
-import "sn-auth/internal/app"
-
-func main() {
-	app.Run("../../configs/local.yaml")
-}
--- sn-auth/migrations/20221018151330_account_management.down.sql
+++
@@ -1,9 +0,0 @@
-drop table if exists operations;
-
-drop table if exists reservations;
-
-drop table if exists products;
-
-drop table if exists accounts;
-
-drop table if exists users;
